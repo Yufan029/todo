946c5555c20a40cd9c56b8e2ea487308b444e7bc
Yufan Zhang <YZhang@rpmglobal.com>
2024-08-10 00:29:06 +1000

    Add header, side today

---
+++ src/createHeader.js
@@ -0,0 +1,19 @@
+import todoIcon from './images/todo-icon.png';
+
+export default function createHeader() {
+    let imageLeft = new Image();
+    imageLeft.src = todoIcon;
+    imageLeft.classList.add('todoIcon');
+    
+    let header = document.querySelector('#header-icon');
+    header.appendChild(imageLeft);
+    
+    let pElem = document.createElement('p');
+    pElem.appendChild(document.createTextNode('To do List'))
+    header.appendChild(pElem);
+    
+    let imageRight = new Image();
+    imageRight.src = todoIcon;
+    imageRight.classList.add('todoIcon');
+    header.appendChild(imageRight);
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './style.css';
+import createHeader from './createHeader';
+import createTodayListItem from './createSideToday';
 
-console.log('testtttttttttttttttt');
+createHeader();
+createTodayListItem();
