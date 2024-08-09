84fe5916d9ae90861b985f63e082345e1f7e0e93
Yufan Zhang <YZhang@rpmglobal.com>
2024-08-09 12:06:45 +1000

    Setting up webpack environment

---
+++ main.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('testtttttttttttttttt');
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
