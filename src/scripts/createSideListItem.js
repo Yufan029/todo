659c2b5861d7ba600fd4266ecee24da9dab60c39
Yufan Zhang <YZhang@rpmglobal.com>
2024-08-12 00:48:53 +1000

    Add clear local storage function

--- src/scripts/createSideListItem.js
+++ src/scripts/createSideListItem.js
@@ -5,12 +5,14 @@ export default function createListItem(itemName, image) {
     let newImage = new Image();
     newImage.src = image;
     newImage.classList.add('side-image');
+    newImage.classList.add(itemName);
 
     sideList.appendChild(newImage);
 
     let pElem = document.createElement('p');
     pElem.appendChild(document.createTextNode(itemName));
     pElem.classList.add('side-text');
+    pElem.classList.add(itemName);
 
     sideList.appendChild(pElem);
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,6 +5,7 @@ import createHeader from './createHeader';
 import createSideListItem from './createSideListItem';
 import todayImage from '../images/today_image.png';
 import projectImage from '../images/project.png';
+import clearImage from '../images/clear.png';
 
 const todoList = localStorage.getItem('todoList');
 console.log(JSON.parse(todoList));
@@ -12,4 +13,11 @@ console.log(JSON.parse(todoList));
 createHeader();
 createSideListItem('Today', todayImage);
 createSideListItem('Projects', projectImage);
+createSideListItem('Clear', clearImage);
+
+const clearBtn = document.querySelector('.side-text.Clear');
+clearBtn.addEventListener('click', () => {
+    localStorage.clear();
+    document.querySelector('main').textContent='';
+});
 
